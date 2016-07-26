9dc04e713b5b3f230da99cc61807802a1f45a000
augisticz <augisticz@gmail.com>
2016-07-26 13:24:52 +0700

    Create form login

--- imports/pages/Register.jsx
+++ imports/pages/Register.jsx
@@ -6,32 +6,32 @@ export default class Register extends React.Component {
       <div className="row register">
         <h4>Register</h4>
         <div className="input-field col l12">
-          <input id="referralName" type="text" class="validate"/>
-          <label for="referralName">Referral Name</label>
+          <input id="referralName" type="text" className="validate"/>
+          <label htmlFor="referralName">Referral Name</label>
         </div>
         <div className="input-field col l12">
-          <input id="firstName" type="text" class="validate"/>
-          <label for="firstName">First Name</label>
+          <input id="firstName" type="text" className="validate"/>
+          <label htmlFor="firstName">First Name</label>
         </div>
         <div className="input-field col l12">
-          <input id="lastName" type="text" class="validate"/>
-          <label for="lastName">Last Name</label>
+          <input id="lastName" type="text" className="validate"/>
+          <label htmlFor="lastName">Last Name</label>
         </div>
         <div className="input-field col l12">
-          <input id="username" type="text" class="validate"/>
-          <label for="username">Username</label>
+          <input id="username" type="text" className="validate"/>
+          <label htmlFor="username">Username</label>
         </div>
         <div className="input-field col l12">
-          <input id="password" type="password" class="validate"/>
-          <label for="password">Password</label>
+          <input id="password" type="password" className="validate"/>
+          <label htmlFor="password">Password</label>
         </div>
         <div className="input-field col l12">
-          <input id="tranactionPin" type="text" class="validate"/>
-          <label for="tranactionPin">Tranaction Pin</label>
+          <input id="tranactionPin" type="text" className="validate"/>
+          <label htmlFor="tranactionPin">Tranaction Pin</label>
         </div>
         <div className="input-field col l12">
-          <input id="email" type="email" class="validate"/>
-          <label for="email">Email</label>
+          <input id="email" type="email" className="validate"/>
+          <label htmlFor="email">Email</label>
         </div>
         <div className="input-field col l12 btn-submit">
           <a className="waves-effect waves-light btn">Submit</a>
--- imports/ui/components/navigator/Navigator.jsx
+++ imports/ui/components/navigator/Navigator.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { $ } from 'meteor/jquery';
 
 export default class Navigator extends React.Component {
+  componentDidMount() {
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 300,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      }
+    );
+
+    $('.btn-submit-login').click(() => {
+      $('.dropdown-button').dropdown('close');
+    })
+  }
 
   onClickLogo() {
     FlowRouter.go('main');
@@ -17,7 +34,7 @@ export default class Navigator extends React.Component {
           <div className="col l6"><span className="tel">+1 800 245 39 251</span></div>
           <div className="col l6 signin-signup">
             <div>
-              <a className="btn-floating btn waves-effect waves-light red"><i className="material-icons">input</i></a>
+              <a className="dropdown-button btn-floating btn waves-effect waves-light red" data-induration="true" data-activates='dropdown1'><i className="material-icons">input</i></a>
               <span>login</span>
               <a onClick={this.onClickSignUp} className="btn-floating btn waves-effect waves-light green"><i className="material-icons">person_pin</i></a>
               <span>register</span>
@@ -39,6 +56,21 @@ export default class Navigator extends React.Component {
             </div>
           </div>
         </div>
+        <ul id='dropdown1' className='dropdown-content active'>
+          <h5>Login</h5>
+          <li className="divider"></li>
+          <div className="input-field col l12">
+            <input id="username" type="text" className="validate"/>
+            <label htmlFor="username">Username</label>
+          </div>
+          <div className="input-field col l12">
+            <input id="password" type="password" className="validate"/>
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className="input-field col l12 btn-submit">
+            <a className="waves-effect waves-light btn btn-submit-login">Submit</a>
+          </div>
+        </ul>
       </div>
     )
   }
